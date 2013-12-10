5d05f702adca283faa283d4d24d98e094105b711
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2013-12-10 11:37:45 +0000

    Fix database creation till we have the final keys

--- interface/conf/evolutions/default/1.sql
+++ interface/conf/evolutions/default/1.sql
@@ -2,24 +2,20 @@
 
 CREATE TABLE public."Teacher"
 (
-  id           SERIAL PRIMARY KEY NOT NULL,
-  name         TEXT               NOT NULL,
-  username     TEXT               NOT NULL,
-  password     TEXT               NOT NULL,
+  "id"           SERIAL PRIMARY KEY NOT NULL,
+  "name"         TEXT               NOT NULL,
+  "username"     TEXT               NOT NULL,
+  UNIQUE("username"),
+  "password"     TEXT               NOT NULL,
   "privateKey" TEXT               NOT NULL,
-  "publicKey"  TEXT               NOT NULL
+  UNIQUE("privateKey"),
+  "publicKey"  TEXT               NOT NULL,
+  UNIQUE("publicKey")
 );
 
-ALTER TABLE public."Teacher"
-ADD CONSTRAINT unique_username UNIQUE (username);
-ALTER TABLE public."Teacher"
-ADD CONSTRAINT "unique_privateKey" UNIQUE ("privateKey");
-ALTER TABLE public."Teacher"
-ADD CONSTRAINT "unique_publicKey" UNIQUE ("publicKey");
-
-INSERT INTO public."Teacher" VALUES (1, 'Rafael', 'ist169801', '5f4dcc3b5aa765d61d8327deb882cf99', '', '');
-INSERT INTO public."Teacher" VALUES (2, 'Rodrigo', 'ist169637', '5f4dcc3b5aa765d61d8327deb882cf99', '', '');
-INSERT INTO public."Teacher" VALUES (3, 'Paulo', 'ist169298', '5f4dcc3b5aa765d61d8327deb882cf99', '', '');
+INSERT INTO public."Teacher" VALUES (1, 'Rafael', 'ist169801', '5f4dcc3b5aa765d61d8327deb882cf99', 'rafael_pvt_key', 'rafael_pub_key');
+INSERT INTO public."Teacher" VALUES (2, 'Rodrigo', 'ist169637', '5f4dcc3b5aa765d61d8327deb882cf99', 'rodrigo_pvt_key', 'rodrigo_pub_key');
+INSERT INTO public."Teacher" VALUES (3, 'Paulo', 'ist169298', '5f4dcc3b5aa765d61d8327deb882cf99', 'paulo_pvt_key', 'paulo_pub_key');
 
 # --- !Downs
 
