47d4f0dba474513d21dfc90b2c0c225aa34bc61f
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2013-12-12 01:55:56 +0000

    Fix script db

--- interface/conf/evolutions/default/1.sql
+++ interface/conf/evolutions/default/1.sql
@@ -2,21 +2,21 @@
 
 CREATE TABLE public."User"
 (
-  "id"       SERIAL PRIMARY KEY NOT NULL,
+  "id"       SERIAL PRIMARY KEY,
   "name"     TEXT               NOT NULL,
   "username" TEXT               NOT NULL,
   "level"    TEXT               NOT NULL,
   UNIQUE ("username")
 );
 
-INSERT INTO public."User" VALUES (1, 'Student 1', 'ist170001', 'Student');
-INSERT INTO public."User" VALUES (2, 'Student 2', 'ist170002', 'Student');
-INSERT INTO public."User" VALUES (3, 'Student 3', 'ist170003', 'Student');
-INSERT INTO public."User" VALUES (4, 'Student 4', 'ist170004', 'Student');
+INSERT INTO public."User" ("name", "username", "level") VALUES ('Student 1', 'ist170001', 'Student');
+INSERT INTO public."User" ("name", "username", "level") VALUES ('Student 2', 'ist170002', 'Student');
+INSERT INTO public."User" ("name", "username", "level") VALUES ('Student 3', 'ist170003', 'Student');
+INSERT INTO public."User" ("name", "username", "level") VALUES ('Student 4', 'ist170004', 'Student');
 
 CREATE TABLE public."UserSecrets"
 (
-  "id"         SERIAL PRIMARY KEY NOT NULL,
+  "id"         SERIAL PRIMARY KEY,
   "userId" BIGINT REFERENCES public."User",
   UNIQUE ("userId"),
   "password"   TEXT               NOT NULL,
@@ -26,43 +26,44 @@ CREATE TABLE public."UserSecrets"
   UNIQUE ("publicKey")
 );
 
-INSERT INTO public."User" VALUES (5, 'Rafael', 'ist169801', 'Admin');
-INSERT INTO public."UserSecrets" VALUES (1, 5, '5f4dcc3b5aa765d61d8327deb882cf99', NULL, NULL);
-INSERT INTO public."User" VALUES (6, 'Rodrigo', 'ist169637', 'Teacher');
-INSERT INTO public."UserSecrets" VALUES (2, 6, '5f4dcc3b5aa765d61d8327deb882cf99', NULL, NULL);
-INSERT INTO public."User" VALUES (7, 'Paulo', 'ist169298', 'Teacher');
-INSERT INTO public."UserSecrets" VALUES (3, 7, '5f4dcc3b5aa765d61d8327deb882cf99', NULL, NULL);
+INSERT INTO public."User" ("name", "username", "level") VALUES ('Rafael', 'ist169801', 'Admin');
+INSERT INTO public."UserSecrets" ("userId", "password", "privateKey", "publicKey") VALUES (5, '5f4dcc3b5aa765d61d8327deb882cf99', NULL, NULL);
+INSERT INTO public."User" ("name", "username", "level") VALUES ('Rodrigo', 'ist169637', 'Teacher');
+INSERT INTO public."UserSecrets" ("userId", "password", "privateKey", "publicKey") VALUES (6, '5f4dcc3b5aa765d61d8327deb882cf99', NULL, NULL);
+INSERT INTO public."User" ("name", "username", "level") VALUES ('Paulo', 'ist169298', 'Teacher');
+INSERT INTO public."UserSecrets" ("userId", "password", "privateKey", "publicKey") VALUES (7, '5f4dcc3b5aa765d61d8327deb882cf99', NULL, NULL);
 
 CREATE TABLE public."Course"
 (
-  "id"         SERIAL PRIMARY KEY NOT NULL,
+  "id"         SERIAL PRIMARY KEY,
   "name"       TEXT               NOT NULL,
   "department" TEXT               NOT NULL
 );
 
-INSERT INTO public."Course" VALUES (1, 'Segurança Informática em Redes e Sistemas', 'DEI');
-INSERT INTO public."Course" VALUES (2, 'Arquitecturas de Software', 'DEI');
+INSERT INTO public."Course" ("name", "department") VALUES ('Segurança Informática em Redes e Sistemas', 'DEI');
+INSERT INTO public."Course" ("name", "department") VALUES ('Arquitecturas de Software', 'DEI');
 
 CREATE TABLE public."Teaching"
 (
-  "id"        SERIAL PRIMARY KEY NOT NULL,
+  "id"        SERIAL PRIMARY KEY,
   "teacherId" BIGINT REFERENCES public."User",
   "classId"   BIGINT REFERENCES public."Course"
 );
 
-INSERT INTO public."Teaching" VALUES (1, 5, 1);
+INSERT INTO public."Teaching" ("teacherId", "classId") VALUES (5, 1);
+INSERT INTO public."Teaching" ("teacherId", "classId") VALUES (6, 2);
 
 CREATE TABLE public."Enrollment"
 (
-  "id"        SERIAL PRIMARY KEY NOT NULL,
+  "id"        SERIAL PRIMARY KEY,
   "studentId" BIGINT REFERENCES public."User",
   "classId"   BIGINT REFERENCES public."Course"
 );
 
-INSERT INTO public."Enrollment" VALUES (1, 1, 1);
-INSERT INTO public."Enrollment" VALUES (2, 2, 1);
-INSERT INTO public."Enrollment" VALUES (3, 3, 1);
-INSERT INTO public."Enrollment" VALUES (4, 4, 2);
+INSERT INTO public."Enrollment" ("studentId", "classId") VALUES (1, 1);
+INSERT INTO public."Enrollment" ("studentId", "classId") VALUES (2, 1);
+INSERT INTO public."Enrollment" ("studentId", "classId") VALUES (3, 1);
+INSERT INTO public."Enrollment" ("studentId", "classId") VALUES (4, 2);
 
 
 # --- !Downs
