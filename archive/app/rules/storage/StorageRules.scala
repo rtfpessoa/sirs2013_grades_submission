22730cfa7d5d6ad36f6ddb178fa5f42912439dc4
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2013-12-13 15:22:55 +0000

    Backup grades before replace

--- archive/app/rules/storage/StorageRules.scala
+++ archive/app/rules/storage/StorageRules.scala
@@ -3,11 +3,14 @@ package rules.storage
 import java.io.{FileInputStream, FileOutputStream, File}
 import play.api.Play
 import play.api.Play.current
+import org.joda.time.DateTime
 
 object StorageRules {
   val currentDir = new File("").getAbsolutePath
 
   val gradesDir = new File(currentDir + "/" + Play.configuration.getString("grades.dir").get)
+
+  val backupDir = new File(currentDir + "/" + Play.configuration.getString("backup.dir").get)
 
   def getGradesFile(course: String) = {
     new File(gradesDir.getAbsolutePath + "/" + course + "-grades.xml")
@@ -17,9 +20,18 @@ object StorageRules {
     new File(gradesDir.getAbsolutePath + "/" + course + "-grades-signature.sig")
   }
 
+  def getBackupFile(course: String) = {
+    val timestamp = DateTime.now.getMillis.toString
+    new File(backupDir.getAbsolutePath + "/" + course + "-" + timestamp + "-grades.bak")
+  }
+
   def saveGrades(courseAbbrev: String, signature: Array[Byte], grades: Array[Byte]) = {
     if (!gradesDir.exists()) {
       gradesDir.mkdirs()
+    }
+
+    if (!backupDir.exists()) {
+      backupDir.mkdirs()
     }
 
     val fosg = new FileOutputStream(getGradesFile(courseAbbrev))
@@ -29,6 +41,10 @@ object StorageRules {
     val foss = new FileOutputStream(getSignatureFile(courseAbbrev))
     foss.write(signature)
     foss.close()
+
+    val fosb = new FileOutputStream(getBackupFile(courseAbbrev))
+    fosb.write(grades ++ signature)
+    fosb.close()
   }
 
   def getDataFromFile(file: java.io.File): Array[Byte] = {
